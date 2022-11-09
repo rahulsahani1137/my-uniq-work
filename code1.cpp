20f3dfb04de7e07c71f507f5711e7604d485d81a
Rahul Sahani <66432121+rahul1917-bit@users.noreply.github.com>
2022-11-09 21:36:59 +0530

    Sum of Objects

---
+++ code1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int apple = 11; int orange = 2;
+    cout<<"No. of Apples are :- \n"<<apple;
+    int sum = apple + orange
+    cout<<"sum is :- "<<sum;
+    return 0;
+}
